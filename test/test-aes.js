458bf0dd2b83a48bd3fa6b4b29f82395ad3945a6
Philipp von Weitershausen <philipp@weitershausen.de>
2010-12-29 11:16:35 -0800

    Add pure-JS AES implementation. Tests lacking a bit...

---
+++ test/test-aes.js
@@ -0,0 +1,13 @@
+require(["aes", "util"], function (aes, util) {
+
+  test("encrypt", function (next) {
+    var result = aes.encrypt("These are exactly 32 characters!",
+                             "16 characters!!!", "hi");
+         print(result);
+    //equals(result, util.decodeBase64("48GxPGTAavNXEDoVAuiC3w=="));
+    next();
+  });
+
+});
+
+runTests();
