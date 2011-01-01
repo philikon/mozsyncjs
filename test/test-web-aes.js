2f2e446a67b4f1633e026550e44e87969fa61937
Philipp von Weitershausen <philipp@weitershausen.de>
2011-01-01 14:25:05 -0800

    now that we have base64, we can enable this. still need more tests, though

--- test/test-web-aes.js
+++ test/test-web-aes.js
@@ -3,8 +3,7 @@ require(["web-aes", "web-util"], function (aes, util) {
   test("encrypt", function (next) {
     var result = aes.encrypt("These are exactly 32 characters!",
                              "16 characters!!!", "hi");
-         print(result);
-    //equals(result, util.decodeBase64("48GxPGTAavNXEDoVAuiC3w=="));
+    equals(util.encodeBase64(result), "48GxPGTAavNXEDoVAuiC3w==");
     next();
   });
 
